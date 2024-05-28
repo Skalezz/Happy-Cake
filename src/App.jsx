bf5eb297d58b1ac346033c9c3202e59b0a3accd3
Esteban Velimir Skarneo Gonzalez <154566477+Skalezz@users.noreply.github.com>
2024-05-27 21:37:54 -0400

    primero

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './views/HomePage'
+import ContactPage from './views/ContactPage'
+import NavBar from './components/NavBar'
+import NotFound from './views/NotFound'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route 
+          path='/'
+          element={<HomePage />}
+        />
+        <Route 
+          path='/contacto'
+          element={<ContactPage />}
+        />
+        <Route 
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/views/HomePage.jsx
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/esm/Container";
+
+const HomePage = () => {
+    return (
+        <Container className="text-center mt-3">
+            <h1>Bienvenido a <strong>Happy Cake!</strong></h1>
+            <p>El lugar de los pasteles Felices</p>
+            <h1>🎂</h1>
+        </Container>
+    )
+};
+
+export default HomePage
